9ba9b2c337917fc815ffbd5d99f78d9db7904493
Timur Rvachov <120140748+trvachov@users.noreply.github.com>
2024-10-22 22:26:21 -0400

    Fix license headers to apache. (#350)
    ## Summary
    Fixing all headers to be Apache license, ahead of open source release.
    
    ## Details
    Should not affect any executed code.

--- bionemo/triton/client_encode.py
+++ bionemo/triton/client_encode.py
@@ -13,18 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-#!/usr/bin/env python3
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
-#
-# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
-# property and proprietary rights in and to this material, related
-# documentation and any modifications thereto. Any use, reproduction,
-# disclosure or distribution of this material and related documentation
-# without an express license agreement from NVIDIA CORPORATION or
-# its affiliates is strictly prohibited.
-
 import json
 from contextlib import ExitStack
 from typing import Callable, Dict, Optional, Sequence
--- tests/test_esm2nv_training_utils.py
+++ tests/test_esm2nv_training_utils.py
@@ -12,19 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-#!/bin/bash
-#
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
-#
-# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
-# property and proprietary rights in and to this material, related
-# documentation and any modifications thereto. Any use, reproduction,
-# disclosure or distribution of this material and related documentation
-# without an express license agreement from NVIDIA CORPORATION or
-# its affiliates is strictly prohibited.
 """
 This file tests the training-related utilities for ESM2.
 """
