10e72bebfb695c9e282ea804a9306352af5c1f37
Malcolm Greaves <mgreaves@nvidia.com>
2024-11-15 17:06:13 -0500

    fix bionemo-fw deps & tach.toml, incorporate bionemo-noodles

--- sub-packages/bionemo-noodles/src/bionemo/noodles/nvfaidx.py
+++ sub-packages/bionemo-noodles/src/bionemo/noodles/nvfaidx.py
@@ -1,4 +1,21 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-Apache2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from noodles_fasta_wrapper import IndexedFastaReader
+
 
 class SequenceAccessor:
     # NOTE: we could totally handle this stuff in Rust if we want.
@@ -15,7 +32,9 @@ class SequenceAccessor:
 
             # Bounds checking for slice
             if start < 0 or stop > self.length:
-                raise IndexError(f"Range [{start}:{stop}] is out of bounds for '{self.seqid}' with length {self.length}.")
+                raise IndexError(
+                    f"Range [{start}:{stop}] is out of bounds for '{self.seqid}' with length {self.length}."
+                )
 
             region_str = f"{self.seqid}:{start + 1}-{stop}"  # +1 for 1-based indexing
             return self.reader.query_region(region_str)
@@ -55,45 +74,47 @@ class NvFaidx:
     def keys(self):
         return self.records.keys()
 
+
 def tests():
-    index = NvFaidx('sample.fasta')
-    print(index['chr1'][1:10])
+    index = NvFaidx("sample.fasta")
+    print(index["chr1"][1:10])
     try:
-        print(index['chr1'][1:10000])
+        print(index["chr1"][1:10000])
     except Exception:
         pass
 
     try:
-        print(index['chr1'][1])
+        print(index["chr1"][1])
     except Exception:
         pass
+
 
 def test_process_parallel_bug():
-    ''' 
-    PyFaidx is a python replacement for faidx that provides a dictionary-like interface to reference genomes. Pyfaidx 
+    """PyFaidx is a python replacement for faidx that provides a dictionary-like interface to reference genomes. Pyfaidx
     is not process safe, and therefore does not play nice with pytorch dataloaders.
 
     Ref: https://github.com/mdshw5/pyfaidx/issues/211
 
-    Naively, this problem can be fixed by keeping index objects private to each process. However, instantiating this object can be quite slow. 
+    Naively, this problem can be fixed by keeping index objects private to each process. However, instantiating this object can be quite slow.
         In the case of hg38, this can take between 20-30 seconds.
 
     For a good solution we need three things:
         1) Safe index creation, in multi-process or multi-node scenarios, this should be restricted to a single node where all workers block until it is complete (not implemented above)
         2) Index object instantion must be fast.
         3) Read-only use of the index object must be both thread safe and process safe with python.
-    '''
+    """
     from multiprocessing import Pool
 
     # NOTE: taken from github issue above, this reproduces the issue with inconsistent reads in parallel.
-    fasta = NvFaidx('hg38.fa')
+    fasta = NvFaidx("hg38.fa")
+
     def read_region(region):
-        res = fasta[region['chr']][region['start']:region['end']]
+        res = fasta[region["chr"]][region["start"] : region["end"]]
         # This fails when using Pyfaidx ephimerally.
         assert len(res) == 10000
 
     # This test is quite slow btw
-    region_list = [{'chr': 'NC_000019.10', 'start': 150000, 'end': 160000} for i in range(10000000)]
+    region_list = [{"chr": "NC_000019.10", "start": 150000, "end": 160000} for i in range(10000000)]
 
     with Pool(processes=16) as pool:
         results = pool.map(read_region, region_list)
@@ -103,9 +124,11 @@ def test_construction_time_hg38():
     # Should safely produce a .fai file in a single process.
     ...
 
+
 def test_parallel_index_creation():
     # Should safely produce a .fai file in a single process.
     ...
+
 
 def test_pyfaidx_equivalence():
     # Should produce the same results as pyfaidx.
