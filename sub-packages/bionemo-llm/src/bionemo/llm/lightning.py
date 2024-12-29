116d2643a9007cf3a66ae7eb366fe9b208b1eaf3
sichu <sichu@nvidia.com>
2024-12-29 02:36:07 +0000

    add verbose output

--- sub-packages/bionemo-llm/src/bionemo/llm/lightning.py
+++ sub-packages/bionemo-llm/src/bionemo/llm/lightning.py
@@ -388,6 +388,7 @@ class BionemoLightningModule(
         print(f"valid_ppl at {self.trainer.global_rank} is {valid_metric_value}.")
         self.log("valid_ppl", valid_metric_value, on_step=False, on_epoch=True)
         self.valid_ppl.reset()
+        print(f"valid_ppl states are total_log_probs={self.valid_ppl.total_log_probs.sum()} and count={self.valid_ppl.count.sum()} at {self.trainer.global_rank} after reset.")
 
 
 def default_megatron_optimizer() -> MegatronOptimizerModule:
