1e7323be2b78a1d34b13a3a25a0c7b1a79cafe69
Dorota Toczydlowska <115542912+dorotat-nv@users.noreply.github.com>
2025-02-28 12:32:28 +0100

    Updates to benchmarks: evo2 (#705)
    ### Description
    Bugfixing and updating evo2 scripts for automated benchmarking execution
    
    ### Type of changes
    <!-- Mark the relevant option with an [x] -->
    
    - [x]  Bug fix (non-breaking change which fixes an issue)
    - [ ]  New feature (non-breaking change which adds functionality)
    - [ ]  Refactor
    - [ ]  Documentation update
    - [ ]  Other (please describe):
    
    ### CI Pipeline Configuration
    Configure CI behavior by applying the relevant labels:
    
    -
    [SKIP_CI](https://github.com/NVIDIA/bionemo-framework/blob/main/docs/docs/user-guide/contributing/contributing.md#skip_ci)
    - Skip all continuous integration tests
    -
    [INCLUDE_NOTEBOOKS_TESTS](https://github.com/NVIDIA/bionemo-framework/blob/main/docs/docs/user-guide/contributing/contributing.md#include_notebooks_tests)
    - Execute notebook validation tests in pytest
    -
    [INCLUDE_SLOW_TESTS](https://github.com/NVIDIA/bionemo-framework/blob/main/docs/docs/user-guide/contributing/contributing.md#include_slow_tests)
    - Execute tests labelled as slow in pytest for extensive testing
    
    
    > [!NOTE]
    > By default, the notebooks validation tests are skipped unless
    explicitly enabled.
    
    ### Usage
    <!--- How does a user interact with the changed code -->
    ```python
    TODO: Add code snippet
    ```
    
    ### Pre-submit Checklist
    <!--- Ensure all items are completed before submitting -->
    
     - [x] I have tested these changes locally
     - [ ] I have updated the documentation accordingly
     - [ ] I have added/updated tests as needed
     - [ ] All existing tests pass successfully

--- sub-packages/bionemo-evo2/src/bionemo/evo2/run/train.py
+++ sub-packages/bionemo-evo2/src/bionemo/evo2/run/train.py
@@ -393,7 +393,9 @@ def train(args: argparse.Namespace):
             tokenizer=tokenizer,
         )
     else:
-        blended_dataset_config = parse_dataset_config(args.dataset_config, args.dataset_dir)
+        blended_dataset_config = parse_dataset_config(
+            dataset_config_path=args.dataset_config, dataset_path=args.dataset_dir
+        )
         dataset_cls = Evo2DatasetPadEodLossMask if args.eod_pad_in_loss_mask else Evo2Dataset
         # Instantiate pre-training module.
         data = PreTrainingDataModule(
