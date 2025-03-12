dca4e2de9bb72168c004bfbfd39c9f4b82c64bd5
dorotat <dorotat@nvidia.com>
2025-03-12 16:16:17 +0100

    updated unit test for training

--- sub-packages/bionemo-evo2/tests/bionemo/evo2/run/test_train.py
+++ sub-packages/bionemo-evo2/tests/bionemo/evo2/run/test_train.py
@@ -46,15 +46,14 @@ def test_train_evo2_runs(tmp_path, num_steps=5):
     # a local copy of the environment
     env = dict(**os.environ)
     env["MASTER_PORT"] = str(open_port)
-    experiment_name = "test"
 
     # Build the command string.
     # Note: The command assumes that `train_evo2` is in your PATH.
     command = (
-        f"train_evo2 --mock-data --result-dir {tmp_path} --experiment-name {experiment_name}"
+        f"train_evo2 --mock-data --result-dir {tmp_path}"
         "--model-size 1b_nv --num-layers 4 --hybrid-override-pattern SDH* "
         "--no-activation-checkpointing --add-bias-output "
-        f"--max-steps {num_steps} --warmup-steps 1 --no-wandb "
+        f"--max-steps {num_steps} --warmup-steps 1"
         "--seq-length 128 --hidden-dropout 0.1 --attention-dropout 0.1 "
     )
 
@@ -94,10 +93,10 @@ def test_train_evo2_stops(tmp_path, num_steps=500000, early_stop_steps=3):
     # Build the command string.
     # Note: The command assumes that `train_evo2` is in your PATH.
     command = (
-        f"train_evo2 --mock-data --experiment-dir {tmp_path}/test_train "
+        f"train_evo2 --mock-data --result-dir {tmp_path}"
         "--model-size 1b_nv --num-layers 4 --hybrid-override-pattern SDH* "
         "--no-activation-checkpointing --add-bias-output "
-        f"--max-steps {num_steps} --early-stop-on-step {early_stop_steps} --warmup-steps 1 --no-wandb "
+        f"--max-steps {num_steps} --early-stop-on-step {early_stop_steps} --warmup-steps 1"
         "--seq-length 128 --hidden-dropout 0.1 --attention-dropout 0.1 "
     )
     command_parts_no_program = shlex.split(command)[1:]
